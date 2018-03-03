9062a556efa5323c9c7f5e03a92d0506eef55156
kyle8285 <kylelussier3@gmail.com>
2018-03-02 21:04:03 -0500

    Fib sum solutions. Should clean up and refactor

---
+++ algorithmic-toolbox/algorithmicWarmUp/fibonacci_sum_again.py
@@ -0,0 +1,25 @@
+# Uses python3
+import sys
+
+def fibonacci_sum(m,n):
+    fib1_mod_10 = 0 % 10
+    fib2_mod_10 = 1 % 10
+    totals = [0,1]
+    while len(totals) >= 0:
+        fib1_mod_10, fib2_mod_10 = fib2_mod_10, ((fib1_mod_10 + fib2_mod_10) % 10)
+        totals.append(fib2_mod_10)
+
+        if fib1_mod_10 == 0 and fib2_mod_10 == 1:
+            totals.pop()
+            totals.pop()
+            break
+
+
+    return ((sum(totals) * (n // len(totals)) + sum(totals[0:(n%len(totals)+1)])) \
+            - (sum(totals) * (m // len(totals)) + sum(totals[0:(m%len(totals))]))) \
+            % 10
+
+if __name__ == '__main__':
+    input = sys.stdin.readline()
+    a,b = map(int, input.split())
+    print(fibonacci_sum(a,b))
